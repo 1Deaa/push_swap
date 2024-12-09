67f842f6770276f5ccb342f8582f02f0a5adf6f8
Deaa Rahwanji <drahwanj@c2r6s3.42amman.com>
2024-12-09 19:59:14 +0300

    push_swap

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: drahwanj <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 15:59:08 by drahwanj          #+#    #+#             */
-/*   Updated: 2024/12/09 19:33:51 by drahwanj         ###   ########.fr       */
+/*   Updated: 2024/12/09 19:58:51 by drahwanj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,10 @@
 void	push_end(Node** root, int value)
 {
 	Node* new_node;
-	Node* curr;
-
+	
 	new_node = malloc(sizeof(Node));
-	if (new_node == NULL)
-	{
-		exit(1);
-	}
-	new_node->next = NULL;
 	new_node->data = value;
-	curr = *root;
-	while (curr->next != NULL)
-	{
-		curr = curr->next;
-	}
-	curr->next = new_node;
+	new_node->next = NULL;
 }
 
 void	create_tree(int length, char **elem)
